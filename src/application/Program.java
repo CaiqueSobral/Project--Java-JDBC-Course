4d162c9763b7c14b498043a7aef8c38ee0ae7451
Caique Sobral <caiquelsobral@gmail.com>
2022-09-28 15:42:30 -0300

    FEAT: Delete data from db

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -11,7 +12,30 @@ public class Program {
 
     public static void main(String[] args) {
 
-        
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "DELETE FROM department "
+                    + "WHERE "
+                    + "Id = ?"
+            );
+
+            st.setInt(1, 5);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        }catch (SQLException e){
+            throw new DbIntegrityException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 
     public void updateData(){
