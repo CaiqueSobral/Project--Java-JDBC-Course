834e5d271f35836d6f69f33209600d5be0105e8d
Caique Sobral <caiquelsobral@gmail.com>
2022-09-27 21:00:50 -0300

    FEAT: insert data into table

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,15 +3,59 @@ package application;
 import db.DB;
 import db.DbException;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyy");
+        Connection conn = null;
+        PreparedStatement st = null;
+
+        try{
+
+            conn = DB.getConnection();
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                    + "VALUES "
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            st.setString(1, "Caique Sobral");
+            st.setString(2, "Caique@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("27/07/2001").getTime()));
+            st.setDouble(4, 4000);
+            st.setInt(5, 1);
+
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0){
+                ResultSet rs =  st.getGeneratedKeys();
+                while (rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            }else{
+                System.out.println("No rows affected");
+            }
+
+        }catch (SQLException e){
+            throw new DbException(e.getMessage());
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+
+    public void getDataFromDb(){
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
