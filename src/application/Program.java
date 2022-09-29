1077263ffe550c79decf71b5bdfd3822a23c10dd
Caique Sobral <caiquelsobral@gmail.com>
2022-09-29 12:04:53 -0300

    FEAT: Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
+import model.entities.Department;
 
 import java.sql.*;
 import java.text.ParseException;
@@ -12,42 +13,12 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Connection conn = null;
-        Statement st = null;
-
-        try{
-            conn = DB.getConnection();
-            conn.setAutoCommit(false);
-
-            st = conn.createStatement();
-
-            int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
-            int x = 1;
-
-            /*
-            if(x < 2){
-                throw new SQLException("Fake error");
-            } */
-            int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-
-            conn.commit(); // Confirm the transaction
-
-            System.out.println("Rows1 " + rows1);
-            System.out.println("Rows2 " + rows2);
-        }catch (SQLException e){
-            try {
-                conn.rollback(); // Reverse the database when error
-                throw new DbException("Transaction rolled back! Caused by " + e.getMessage());
-            } catch (SQLException ex) {
-                throw new DbException("Error trying to rollback! Caused by " + ex.getMessage());
-            }
-        }
-        finally {
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
+}
 
+    /*
     public void deleteData(){
         Connection conn = null;
         PreparedStatement st = null;
@@ -173,4 +144,6 @@ public class Program {
             DB.closeConnection();
         }
     }
-}
+
+
+} */
